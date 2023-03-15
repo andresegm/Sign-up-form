ca6c12f1c9a2392f1cd53a32ce6ad960dc7e178f
Andres Godoy <andresegm@gmail.com>
2023-03-15 16:07:31 -0600

    add validate function to JS to validate password matches

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+function Validate() {
+const pass = document.querySelector('.pass').value;
+const passC = document.querySelector('.passC').value;
+
+    if (pass !== passC) {
+        console.log('passwords do not match');
+    }
+}
